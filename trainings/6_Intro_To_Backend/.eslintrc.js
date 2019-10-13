da887cf129d6fbfeb6326e52ee4c5e01182b2490
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-10-13 12:49:09 -0500

    Add introduction to backend tutorial

---
+++ trainings/6_Intro_To_Backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
